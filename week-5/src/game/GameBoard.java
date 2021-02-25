5721f858283fb56b2b5c860f2ee635cf7201263b
KrasimiraVeleva <63585462+KrasimiraVeleva@users.noreply.github.com>
2021-02-25 23:18:13 +0200

    Add files via upload

--- week-5/src/game/GameBoard.java
+++ week-5/src/game/GameBoard.java
@@ -2,50 +2,37 @@ package game;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
-public class GameBoard extends JFrame implements MouseListener {
+public class GameBoard extends JFrame {
 
     public static final int TILE_SIDE_COUNT = 64;
     private GameTile[][] tiles = new GameTile[64][54];
     private GameTile chosenTile;
 
     public GameBoard() {
-        this.setSize(660, 660);
+
         this.setVisible(true);
-        this.addMouseListener(this);
+        this.setSize(660, 660);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
+    /**
+     * @autor Красимира Велева
+     * Метод визуализиращ полетата на игралната дъска
+     * @param g Graphics object
+     */
     public void paint(Graphics g) {
         super.paint(g);
         for(int row = 0; row < 64; row++) {
             for(int col = 0; col < 64; col++) {
-                GameTile tiles = new GameTile(row, col, Color.BLACK);
+                GameTile tiles = new GameTile(row, col);
                 tiles.boardGrid(g);
+                tiles.render(g);
+                tiles.BurnedPixel(g);
+                tiles.AboutToBurnedPixel(g);
+                tiles.SturdyPixel(g);
+
             }
-
         }
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
     }
 }
--- week-5/src/game/GameTile.java
+++ week-5/src/game/GameTile.java
@@ -6,30 +6,45 @@ import java.awt.*;
 public class GameTile extends JFrame {
 
     public static final int TILE_SIZE = 10;
-    private Color color;
     private int row;
     private int col;
 
-    public GameTile(int row, int col, Color color) {
+    public GameTile(int row, int col) {
+
         this.row = row;
         this.col = col;
-        this.color = color;
     }
 
-    public void render(Graphics g) {
-        for(int x = 0; x < 640; x++) {
-            for(int y = 0; y < 640; y++) {
+    void render(Graphics g) {
+        for (int x = 0; x < 640; x++) {
+            for (int y = 0; y < 640; y++) {
+
                 boardGrid(g);
+                BurnedPixel(g);
+                AboutToBurnedPixel(g);
+                SturdyPixel(g);
             }
         }
     }
 
     void boardGrid(Graphics g) {
         g.setColor(Color.BLACK);
-        for(int i = 0; i <= 640; i += 10) {
-            for(int j = 50; j <= 640; j += 10) {
+        for (int i = 0; i <= 640; i += 10) {
+            for (int j = 30; j <= 640; j += 10) {
                 g.drawRect(i, j, 100, 100);
             }
         }
+    }
+
+    public void BurnedPixel(Graphics g) {
+        g.setColor(Color.RED);
+    }
+
+    public void AboutToBurnedPixel(Graphics g) {
+        g.setColor(Color.GREEN);
+    }
+
+    public void SturdyPixel(Graphics g) {
+        g.setColor(Color.BLUE);
     }
 }
